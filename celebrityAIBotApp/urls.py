21e95027d471c336d5ad4c07c2e5d013243d90ae
mayababu <mayababu73@gmail.com>
2023-04-27 19:41:21 +1200

    Creates json file, added comments

--- celebrityAIBotApp/urls.py
+++ celebrityAIBotApp/urls.py
@@ -3,7 +3,8 @@ from celebrityAIBotApp import views
 
 app_name = 'celeb_ai'
 
-urlpatterns = [
-    path('', views.celebrity_ai_view, name='celeb_ai_view'),   
-    path('apology', views.apology_message_view, name='apology_message_view'),
+urlpatterns = [ #list of url paths the app may need to go through
+    path('', views.celebrity_ai_view, name='celeb_ai_view'), # creating a path to the page that needs to be displayed
+    path('apology', views.apology_message_view, name='apology_message_view'), # creating a path to the page that need to be displaeyd if the user
+                                                                            # goes over the limit for the number of questions asked for a day
 ]
--- generate_index_file.py
+++ generate_index_file.py
@@ -1,19 +1,30 @@
+# GENERATING INDEX FILE
+#importing libraries needed - os is pre-installed
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
+from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
+import os
 
 if __name__ == "__main__":
     try:
         # get api key from azure keyvault
         kv_uri = "https://kv-celeb-ai-chatbot.vault.azure.net/"
-        kv_name = "kv-celeb-ai-chatbot"
+        kv_name = "kv-celeb-ai-chatbot" # name of the keyvault
         secret_name = "OPENAI-API-KEY"
         credential = DefaultAzureCredential()
         client = SecretClient(vault_url=kv_uri, credential=credential)
 
         openai_api_key = client.get_secret(secret_name).value
+        os.environ["OPENAI_API_KEY"] = openai_api_key # setting api key in the environment as an environemnt variable
+
+        documents = SimpleDirectoryReader('docs').load_data() # loading the data from the docs folder to genertae the index json file
+
+        index = GPTSimpleVectorIndex.from_documents(documents) # creating index
+
+        index.save_to_disk('data/actor_index.json') #creating a json file under the folder called data. this json file is what OPENAI will use 
 
     except Exception as error:
-        raise error
+        raise error # raising potential errors if there is one
     
     finally:
         print(f":D")
